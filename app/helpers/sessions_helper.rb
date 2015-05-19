c94496ebe5407a3da1b851b5fbfeb756d3488fc6
zmingshi <zmingshi@gmail.com>
2015-05-19 19:13:46 +0800

    cancel CSRF token authenticity

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,20 @@
 module SessionsHelper
+  
+  def sign_in(user)
+    self.current_user = user
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= user_from_id
+  end
+
+  private
+    def user_from_id
+	  User.find_by(id: session[:user_id])
+	end
+
 end
