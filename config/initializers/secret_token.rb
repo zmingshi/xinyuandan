0bee6644217eb864936a0a6a287fa5fbcfd927ca
zmingshi <zmingshi@gmail.com>
2015-05-19 09:12:09 +0800

    xinyuandan: create user

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xinyuandan::Application.config.secret_key_base = 'a48e48aa8f8f1b1a0e04b920925bf7884099324799648e9d36bb6c82d664edf4ea5c460e6132fb77c9f301801ad50ee552aa930a9e33506609aca26ea7e3cd89'
